1766f22538b3c19a2e4bd133d8ad6276b4b2b3d8
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-06-29 22:14:30 -0400

    commit tree 09af0954d55133d0cfc6d10e8f3f6dd096a540bf

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -9,6 +9,21 @@ function Question({ question, onAnswered }) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+  useEffect(() => {
+    let timerId = 0
+    if (timeRemaining > 0) {
+      timerId = setTimeout(() => {
+        setTimeRemaining(timeRemaining => timeRemaining - 1)
+      }, 1000)
+    } else {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+
+    return (() => clearTimeout(timerId))
+
+  }, [timeRemaining])
 
   const { id, prompt, answers, correctIndex } = question;
 
